1fbefd8e1f225347996c7d44bd1b1bcedce2ae15
gh0sty <80682417+1lexaa@users.noreply.github.com>
2022-06-06 17:44:02 +0300

    Delete 2DIGIT.sql

--- 2DIGIT.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT 
-	CONCAT( Faces.Name, ' ', Suits.Name ) AS PlayingCards
-FROM 
-	(VALUES ('6'), ('7'), ('8'), ('9'), ('10'), ('J'), ('Q'), ('K'), ('A')) AS Faces(Name),
-	(VALUES ('Speak'), ('Clubs'), ('Diamonds'), ('Hearts')) AS Suits(Name)
-ORDER BY 
-	NEWID()
